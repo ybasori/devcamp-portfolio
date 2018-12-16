9c1f071b75fbd7b7a5cfb6eb87d1d88e8442c512
Yusuf <ybasori@gmail.com>
2018-12-16 23:09:57 +0700

    implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-5.times do |topic|
+3.times do |topic|
 	Topic.create!(
 		title: "Topic #{topic}"
 	)
 end
+
+puts "3 topics created"
 
 10.times do |i|
 	Blog.create!(
@@ -31,10 +33,20 @@ end
 
 puts "5 skills created"
 
-9.times do |k|
+8.times do |k|
 	Portfolio.create!(
 		title: "Portfolio title #{k}",
-		subtitle: "My geat service",
+		subtitle: "Ruby on Rails",
+		body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+		main_image: "https://via.placeholder.com/600x400",
+		thumb_image: "https://via.placeholder.com/350x200"
+	)
+end
+
+1.times do |k|
+	Portfolio.create!(
+		title: "Portfolio title #{k}",
+		subtitle: "Angular",
 		body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 		main_image: "https://via.placeholder.com/600x400",
 		thumb_image: "https://via.placeholder.com/350x200"
